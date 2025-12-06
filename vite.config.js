f39063ae1ec6c0b8151e293c95c45076150ea5ed
AIChoubeyX <AIChoubeyX@users.noreply.github.com>
2025-12-06 23:11:47 +0530

    Update output directory path in Vercel configuration and add build settings in Vite config

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,15 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'], // prevents vite from pre-optimizing lucide-react
+  },
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true,
   },
 });
